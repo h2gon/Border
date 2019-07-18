48a3d53459746d3208755b30a0447bf43c74eaf8
h2gon <h2__gon@naver.com>
2019-07-18 17:50:16 +0900

    Security 추가

--- src/main/java/com/example/demo/repository/MemberRepository.java
+++ src/main/java/com/example/demo/repository/MemberRepository.java
@@ -8,4 +8,6 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface MemberRepository extends JpaRepository<Member, Integer>{
 
+	Member findByuserName(String username);
+
 }
--- src/main/java/com/example/demo/service/MemberService.java
+++ src/main/java/com/example/demo/service/MemberService.java
@@ -1,22 +1,45 @@
 package com.example.demo.service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import com.example.demo.entity.Member;
 import com.example.demo.repository.MemberRepository;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-
 @Service
 @Transactional
-public class MemberService{
+public class MemberService implements UserDetailsService {
 
     private final MemberRepository memberRepository;
+    private final PasswordEncoder passwordEncoder;
 
-    public MemberService(MemberRepository memberRepository){
+    public MemberService(MemberRepository memberRepository, PasswordEncoder passwordEncoder) {
         this.memberRepository = memberRepository;
-    }    
+        this.passwordEncoder = passwordEncoder;
+    }
 
-    public void join(Member member){
+    public void join(Member member) {
+        member.setUserPassword(passwordEncoder.encode(member.getUserPassword()));
         memberRepository.save(member);
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Member member = memberRepository.findByuserName(username);
+
+        List<GrantedAuthority> authorities = new ArrayList<>();
+        authorities.add(new SimpleGrantedAuthority("ROLE_USER"));
+
+        return new User(member.getUserName(), member.getUserPassword(), authorities);
     }
 }
